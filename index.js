73474d1644c2b3c94e41ae5122dd89a9ecffc4d7
Mikhail Mikheev <mdoublemmm@gmail.com>
2018-07-13 18:29:43 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+import styles from './styles.css'
+import './styles.css'
+
+const APP_ID = process.env.APP_ID || 'UA-XXXXX-Y'
+
+const getCookie = cname => {
+  const match = document.cookie.match('(^|;) ?' + cname + '=([^;]*)(;|$)')
+  return match ? match[2] : ''
+}
+
+const consentCookie = getCookie('consentCookie') // Initialise a variable to store the consent cookie
+
+const GOOGLE_ANALYTICS_URL = 'https://www.google-analytics.com/analytics.js'
+
+const bannerHTML = `
+    <div class="${styles.cookieBar}">
+        <span class="${styles.annotation}">We use cookies to give you the best experience and to help improve our website. Please read our <a href="${process.env.cookiesPolicyLink}" target="_blank" class="${styles.policyLink}">Cookie Policy</a>
+            for more information. By clicking “Accept Cookies,” you agree to the storing of cookies on your device to
+            enhance site navigation and analyze site usage.</span>
+        <div class="${styles.buttonContainer}">
+            <button id="acceptButton" class="${styles.button} ${styles.accept}">Accept Cookies</button>
+            <button id="disableButton" class="${styles.button} ${styles.disable}">Disable Cookies</button>
+        </div>
+    </div>`
+
+const banner = document.createElement('div')
+banner.innerHTML = bannerHTML
+
+document.onload = getConsent()
+
+function loadGA() {
+  const script = document.createElement('script')
+  script.src = GOOGLE_ANALYTICS_URL
+  script.type = 'text/javascript'
+  document.head.appendChild(script)
+
+  script.onload = () => {
+    ga('create', APP_ID, 'auto')
+    ga('send', 'pageview')
+  }
+}
+
+function writeCookie(key, value, days) {
+    var date = new Date();
+
+    let expirationDate
+    if (days) {
+      // Get unix milliseconds at current time plus number of days
+      expirationDate = date.setTime(+ date + (days * 86400000)).toGMTString() //24 * 60 * 60 * 1000
+    }
+    console.log(days)
+    document.cookie = `${key}=${value};${expirationDate ? expirationDate : ''}; path=/`
+};
+
+function hideBanner() {
+  banner.firstChild.nextSibling.style.animationName = styles['float-up']
+  setTimeout(() => {
+    banner.style.display = 'none'
+  }, 1000)
+}
+
+function getConsent() {
+  if (consentCookie == '') {
+    document.body.appendChild(banner)
+
+    const acceptButton = document.getElementById('acceptButton')
+    const disableButton = document.getElementById('disableButton')
+
+    acceptButton.onclick = () => {
+      loadGA()
+      hideBanner()
+      writeCookie('consentCookie', 'yes', process.env.acceptedCookieExpiration)
+    }
+
+    disableButton.onclick = () => {
+      writeCookie('consentCookie', 'no', process.env.disabledCookieExpiration)
+      hideBanner()
+    }
+  }
+}
